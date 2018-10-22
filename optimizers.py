1a57e3de1e72b62616063c01b4a3c0868d71bcbe
marvis <marvis@localhost.localdomain>
2018-10-22 20:46:09 +0300

    few optimizers

--- optimizers.py
+++ optimizers.py
@@ -1,3 +1,69 @@
 """
 This module contains the gradient-based optimizers
 """
+
+from math import sqrt
+lr = 0.001
+
+
+class Optimizer:
+    def __init__(self, mode, f, start_pos):
+        # function to minimize
+        self.f = f
+        # starting position for finding the minimum
+        self.state = start_pos
+        # optimizing mode, one of the below functions
+        self.mode = mode
+        # v and s start at (0,0)
+        self.v = (0,0)
+        self.s = (0,0)
+
+    def step(self):
+        return self.mode(self)
+
+
+
+def SGD(optimizer):
+    #TODO
+    new_state = optimizer.state
+    return new_state
+
+
+def AdaGrad(optimizer):
+    #TODO
+    new_state = optimizer.state
+    return new_state
+
+
+def NesterovMomentum(optimizer, miu=0.9):
+    v = optimizer.v
+    f = optimizer.f
+    state = optimizer.state
+    optimizer.v = miu * v + lr * f(state, derivative=True)
+    new_state = state - optimizer.v
+    return new_state
+
+
+def RMSProp(optimizer, beta=0.1, eps=10**(-8)):
+    state = optimizer.state
+    g = optimizer.f(state, derivative=True)
+    optimizer.s = (1 - beta) * optimizer.s + beta * g*g
+    new_state = state - lr * g / (sqrt(s) + eps)
+    return new_state
+
+
+def Adam(optimizer, eps=10**(-8), beta1=0.9, beta2=0.999):
+    v = optimizer.v
+    s = optimizer.s
+    state = optimizer.state
+    g = optimizer.f(state, derivative=True)
+
+    optimizer.v = beta1 * v + (1. - beta1) * g
+    optimizer.s = beta2 * s + (1. - beta2) * g*g
+
+    v_unbiased = v / (1. - beta1)
+    s_unbiased = s / (1. - beta2)
+
+    new_state = state - lr * v_unbiased / (sqrt(s_unbiased) + eps)
+    return new_state
+
--- test_functions.py
+++ test_functions.py
@@ -1,3 +1,11 @@
 """
 This module contains RxR -> R functions to test the optimizers on
 """
+
+def f1(state, derivative=False):
+    if derivate:
+        #TODO: evaluate derivative of function in state
+        pass
+    else:
+        #TODO: evaluate function in state
+        pass
