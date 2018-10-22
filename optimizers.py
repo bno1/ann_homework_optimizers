8179d5ce25939c8ca24b35043e00d6af2902f341
marvis <marvis@localhost.localdomain>
2018-10-22 22:48:17 +0300

    optimizers fix

--- optimizers.py
+++ optimizers.py
@@ -2,8 +2,8 @@
 This module contains the gradient-based optimizers
 """
 
-from math import sqrt
-lr = 0.001
+import numpy as np
+lr = 0.01
 
 
 class Optimizer:
@@ -16,8 +16,8 @@ class Optimizer:
         # optimizing mode, one of the below functions
         self.mode = mode
         # v and s start at (0,0)
-        self.v = (0, 0)
-        self.s = (0, 0)
+        self.v = np.array([0, 0])
+        self.s = np.array([0, 0])
 
     def step(self):
         self.state = self.mode(self)
@@ -39,6 +39,8 @@ def NesterovMomentum(optimizer, miu=0.9):
     v = optimizer.v
     f = optimizer.f
     state = optimizer.state
+
+    ev = lr * f(state, derivative=True)
     optimizer.v = miu * v + lr * f(state, derivative=True)
     new_state = state - optimizer.v
     return new_state
@@ -48,7 +50,7 @@ def RMSProp(optimizer, beta=0.1, eps=10**(-8)):
     state = optimizer.state
     g = optimizer.f(state, derivative=True)
     optimizer.s = (1 - beta) * optimizer.s + beta * g*g
-    new_state = state - lr * g / (sqrt(optimizer.s) + eps)
+    new_state = state - lr * g / (np.sqrt(np.asarray(optimizer.s)) + eps)
     return new_state
 
 
@@ -64,6 +66,6 @@ def Adam(optimizer, eps=10**(-8), beta1=0.9, beta2=0.999):
     v_unbiased = v / (1. - beta1)
     s_unbiased = s / (1. - beta2)
 
-    new_state = state - lr * v_unbiased / (sqrt(s_unbiased) + eps)
+    new_state = state - lr * v_unbiased / (np.sqrt(s_unbiased) + eps)
     return new_state
 
